67d48e5cce6160804b412fe85564d7bb7e587dcd
Dimitri Williams <dimtri.williams.6448@gmail.com>
2024-02-16 22:30:30 -0600

    Feat: Created quiz template for graphql.

---
+++ src/data/graphql.js
@@ -0,0 +1,629 @@
+const test1 = {
+  questions: [
+    {
+      question: {
+        number: 1,
+        headerTxt: "",
+        codeBlock: ``,
+        footerTxt: "",
+      },
+      answers: [
+        {
+          letter: "A",
+          answersTxt: ``,
+          isCodeBlock: false,
+        },
+        {
+          letter: "B",
+          answersTxt: ``,
+          isCodeBlock: false,
+        },
+        {
+          letter: "C",
+          answersTxt: ``,
+          isCodeBlock: false,
+        },
+        {
+          letter: "D",
+          answersTxt: ``,
+          isCodeBlock: false,
+        },
+      ],
+      correctAnswer: "",
+    },
+    {
+      question: {
+        number: 2,
+        headerTxt: "",
+        codeBlock: ``,
+        footerTxt: "",
+      },
+      answers: [
+        {
+          letter: "A",
+          answersTxt: ``,
+          isCodeBlock: false,
+        },
+        {
+          letter: "B",
+          answersTxt: ``,
+          isCodeBlock: false,
+        },
+        {
+          letter: "C",
+          answersTxt: ``,
+          isCodeBlock: false,
+        },
+        {
+          letter: "D",
+          answersTxt: ``,
+          isCodeBlock: false,
+        },
+      ],
+      correctAnswer: "",
+    },
+    {
+      question: {
+        number: 3,
+        headerTxt: "",
+        codeBlock: ``,
+        footerTxt: "",
+      },
+      answers: [
+        {
+          letter: "A",
+          answersTxt: ``,
+          isCodeBlock: false,
+        },
+        {
+          letter: "B",
+          answersTxt: ``,
+          isCodeBlock: false,
+        },
+        {
+          letter: "C",
+          answersTxt: ``,
+          isCodeBlock: false,
+        },
+        {
+          letter: "D",
+          answersTxt: ``,
+          isCodeBlock: false,
+        },
+      ],
+      correctAnswer: "",
+    },
+    {
+      question: {
+        number: 4,
+        headerTxt: "",
+        codeBlock: ``,
+        footerTxt: "",
+      },
+      answers: [
+        {
+          letter: "A",
+          answersTxt: ``,
+          isCodeBlock: false,
+        },
+        {
+          letter: "B",
+          answersTxt: ``,
+          isCodeBlock: false,
+        },
+        {
+          letter: "C",
+          answersTxt: ``,
+          isCodeBlock: false,
+        },
+        {
+          letter: "D",
+          answersTxt: ``,
+          isCodeBlock: false,
+        },
+      ],
+      correctAnswer: "",
+    },
+    {
+      question: {
+        number: 5,
+        headerTxt: "",
+        codeBlock: ``,
+        footerTxt: "",
+      },
+      answers: [
+        {
+          letter: "A",
+          answersTxt: ``,
+          isCodeBlock: false,
+        },
+        {
+          letter: "B",
+          answersTxt: ``,
+          isCodeBlock: false,
+        },
+        {
+          letter: "C",
+          answersTxt: ``,
+          isCodeBlock: false,
+        },
+        {
+          letter: "D",
+          answersTxt: ``,
+          isCodeBlock: false,
+        },
+      ],
+      correctAnswer: "",
+    },
+    {
+      question: {
+        number: 6,
+        headerTxt: "",
+        codeBlock: ``,
+        footerTxt: "",
+      },
+      answers: [
+        {
+          letter: "A",
+          answersTxt: ``,
+          isCodeBlock: false,
+        },
+        {
+          letter: "B",
+          answersTxt: ``,
+          isCodeBlock: false,
+        },
+        {
+          letter: "C",
+          answersTxt: ``,
+          isCodeBlock: false,
+        },
+        {
+          letter: "D",
+          answersTxt: ``,
+          isCodeBlock: false,
+        },
+      ],
+      correctAnswer: "",
+    },
+    {
+      question: {
+        number: 7,
+        headerTxt: "",
+        codeBlock: ``,
+        footerTxt: "",
+      },
+      answers: [
+        {
+          letter: "A",
+          answersTxt: ``,
+          isCodeBlock: false,
+        },
+        {
+          letter: "B",
+          answersTxt: ``,
+          isCodeBlock: false,
+        },
+        {
+          letter: "C",
+          answersTxt: ``,
+          isCodeBlock: false,
+        },
+        {
+          letter: "D",
+          answersTxt: ``,
+          isCodeBlock: false,
+        },
+      ],
+      correctAnswer: "",
+    },
+    {
+      question: {
+        number: 8,
+        headerTxt: "",
+        codeBlock: ``,
+        footerTxt: "",
+      },
+      answers: [
+        {
+          letter: "A",
+          answersTxt: ``,
+          isCodeBlock: false,
+        },
+        {
+          letter: "B",
+          answersTxt: ``,
+          isCodeBlock: false,
+        },
+        {
+          letter: "C",
+          answersTxt: ``,
+          isCodeBlock: false,
+        },
+        {
+          letter: "D",
+          answersTxt: ``,
+          isCodeBlock: false,
+        },
+      ],
+      correctAnswer: "",
+    },
+    {
+      question: {
+        number: 9,
+        headerTxt: "",
+        codeBlock: ``,
+        footerTxt: "",
+      },
+      answers: [
+        {
+          letter: "A",
+          answersTxt: ``,
+          isCodeBlock: false,
+        },
+        {
+          letter: "B",
+          answersTxt: ``,
+          isCodeBlock: false,
+        },
+        {
+          letter: "C",
+          answersTxt: ``,
+          isCodeBlock: false,
+        },
+        {
+          letter: "D",
+          answersTxt: ``,
+          isCodeBlock: false,
+        },
+      ],
+      correctAnswer: "",
+    },
+    {
+      question: {
+        number: 10,
+        headerTxt: "",
+        codeBlock: ``,
+        footerTxt: "",
+      },
+      answers: [
+        {
+          letter: "A",
+          answersTxt: ``,
+          isCodeBlock: false,
+        },
+        {
+          letter: "B",
+          answersTxt: ``,
+          isCodeBlock: false,
+        },
+        {
+          letter: "C",
+          answersTxt: ``,
+          isCodeBlock: false,
+        },
+        {
+          letter: "D",
+          answersTxt: ``,
+          isCodeBlock: false,
+        },
+      ],
+      correctAnswer: "",
+    },
+    {
+      question: {
+        number: 11,
+        headerTxt: "",
+        codeBlock: ``,
+        footerTxt: "",
+      },
+      answers: [
+        {
+          letter: "A",
+          answersTxt: ``,
+          isCodeBlock: false,
+        },
+        {
+          letter: "B",
+          answersTxt: ``,
+          isCodeBlock: false,
+        },
+        {
+          letter: "C",
+          answersTxt: ``,
+          isCodeBlock: false,
+        },
+        {
+          letter: "D",
+          answersTxt: ``,
+          isCodeBlock: false,
+        },
+      ],
+      correctAnswer: "",
+    },
+    {
+      question: {
+        number: 12,
+        headerTxt: "",
+        codeBlock: ``,
+        footerTxt: "",
+      },
+      answers: [
+        {
+          letter: "A",
+          answersTxt: ``,
+          isCodeBlock: false,
+        },
+        {
+          letter: "B",
+          answersTxt: ``,
+          isCodeBlock: false,
+        },
+        {
+          letter: "C",
+          answersTxt: ``,
+          isCodeBlock: false,
+        },
+        {
+          letter: "D",
+          answersTxt: ``,
+          isCodeBlock: false,
+        },
+      ],
+      correctAnswer: "",
+    },
+    {
+      question: {
+        number: 13,
+        headerTxt: "",
+        codeBlock: ``,
+        footerTxt: "",
+      },
+      answers: [
+        {
+          letter: "A",
+          answersTxt: ``,
+          isCodeBlock: false,
+        },
+        {
+          letter: "B",
+          answersTxt: ``,
+          isCodeBlock: false,
+        },
+        {
+          letter: "C",
+          answersTxt: ``,
+          isCodeBlock: false,
+        },
+        {
+          letter: "D",
+          answersTxt: ``,
+          isCodeBlock: false,
+        },
+      ],
+      correctAnswer: "",
+    },
+    {
+      question: {
+        number: 14,
+        headerTxt: "",
+        codeBlock: ``,
+        footerTxt: "",
+      },
+      answers: [
+        {
+          letter: "A",
+          answersTxt: ``,
+          isCodeBlock: false,
+        },
+        {
+          letter: "B",
+          answersTxt: ``,
+          isCodeBlock: false,
+        },
+        {
+          letter: "C",
+          answersTxt: ``,
+          isCodeBlock: false,
+        },
+        {
+          letter: "D",
+          answersTxt: ``,
+          isCodeBlock: false,
+        },
+      ],
+      correctAnswer: "",
+    },
+    {
+      question: {
+        number: 15,
+        headerTxt: "",
+        codeBlock: ``,
+        footerTxt: "",
+      },
+      answers: [
+        {
+          letter: "A",
+          answersTxt: ``,
+          isCodeBlock: false,
+        },
+        {
+          letter: "B",
+          answersTxt: ``,
+          isCodeBlock: false,
+        },
+        {
+          letter: "C",
+          answersTxt: ``,
+          isCodeBlock: false,
+        },
+        {
+          letter: "D",
+          answersTxt: ``,
+          isCodeBlock: false,
+        },
+      ],
+      correctAnswer: "",
+    },
+    {
+      question: {
+        number: 16,
+        headerTxt: "",
+        codeBlock: ``,
+        footerTxt: "",
+      },
+      answers: [
+        {
+          letter: "A",
+          answersTxt: ``,
+          isCodeBlock: false,
+        },
+        {
+          letter: "B",
+          answersTxt: ``,
+          isCodeBlock: false,
+        },
+        {
+          letter: "C",
+          answersTxt: ``,
+          isCodeBlock: false,
+        },
+        {
+          letter: "D",
+          answersTxt: ``,
+          isCodeBlock: false,
+        },
+      ],
+      correctAnswer: "",
+    },
+    {
+      question: {
+        number: 17,
+        headerTxt: "",
+        codeBlock: ``,
+        footerTxt: "",
+      },
+      answers: [
+        {
+          letter: "A",
+          answersTxt: ``,
+          isCodeBlock: false,
+        },
+        {
+          letter: "B",
+          answersTxt: ``,
+          isCodeBlock: false,
+        },
+        {
+          letter: "C",
+          answersTxt: ``,
+          isCodeBlock: false,
+        },
+        {
+          letter: "D",
+          answersTxt: ``,
+          isCodeBlock: false,
+        },
+      ],
+      correctAnswer: "",
+    },
+    {
+      question: {
+        number: 18,
+        headerTxt: "",
+        codeBlock: ``,
+        footerTxt: "",
+      },
+      answers: [
+        {
+          letter: "A",
+          answersTxt: ``,
+          isCodeBlock: false,
+        },
+        {
+          letter: "B",
+          answersTxt: ``,
+          isCodeBlock: false,
+        },
+        {
+          letter: "C",
+          answersTxt: ``,
+          isCodeBlock: false,
+        },
+        {
+          letter: "D",
+          answersTxt: ``,
+          isCodeBlock: false,
+        },
+      ],
+      correctAnswer: "",
+    },
+    {
+      question: {
+        number: 19,
+        headerTxt: "",
+        codeBlock: ``,
+        footerTxt: "",
+      },
+      answers: [
+        {
+          letter: "A",
+          answersTxt: ``,
+          isCodeBlock: false,
+        },
+        {
+          letter: "B",
+          answersTxt: ``,
+          isCodeBlock: false,
+        },
+        {
+          letter: "C",
+          answersTxt: ``,
+          isCodeBlock: false,
+        },
+        {
+          letter: "D",
+          answersTxt: ``,
+          isCodeBlock: false,
+        },
+      ],
+      correctAnswer: "",
+    },
+    {
+      question: {
+        number: 20,
+        headerTxt: "",
+        codeBlock: ``,
+        footerTxt: "",
+      },
+      answers: [
+        {
+          letter: "A",
+          answersTxt: ``,
+          isCodeBlock: false,
+        },
+        {
+          letter: "B",
+          answersTxt: ``,
+          isCodeBlock: false,
+        },
+        {
+          letter: "C",
+          answersTxt: ``,
+          isCodeBlock: false,
+        },
+        {
+          letter: "D",
+          answersTxt: ``,
+          isCodeBlock: false,
+        },
+      ],
+      correctAnswer: "",
+    },
+  ],
+};
+
+export default {
+  name: "Graphql",
+  tests: [test1],
+};
