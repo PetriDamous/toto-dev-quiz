7cf83064e5e35813a9e1f72b1fa1db1914348b7e
Dimitri Williams <dimtri.williams.6448@gmail.com>
2024-02-16 18:55:26 -0600

    Feat: Created data folder, shell for first test, and a template file.

---
+++ src/data/javascript.js
@@ -0,0 +1,267 @@
+const test1 = {
+  question1: {
+    question: {
+      number: "",
+      headerTxt: "",
+      codeBlock: ``,
+      footerTxt: "",
+    },
+    answers: {
+      letter: "",
+      answersTxt: ``,
+      isCodeBlock: "",
+    },
+  },
+  question2: {
+    question: {
+      number: "",
+      headerTxt: "",
+      codeBlock: ``,
+      footerTxt: "",
+    },
+    answers: {
+      letter: "",
+      answersTxt: ``,
+      isCodeBlock: "",
+    },
+  },
+  question3: {
+    question: {
+      number: "",
+      headerTxt: "",
+      codeBlock: ``,
+      footerTxt: "",
+    },
+    answers: {
+      letter: "",
+      answersTxt: ``,
+      isCodeBlock: "",
+    },
+  },
+  question4: {
+    question: {
+      number: "",
+      headerTxt: "",
+      codeBlock: ``,
+      footerTxt: "",
+    },
+    answers: {
+      letter: "",
+      answersTxt: ``,
+      isCodeBlock: "",
+    },
+  },
+  question5: {
+    question: {
+      number: "",
+      headerTxt: "",
+      codeBlock: ``,
+      footerTxt: "",
+    },
+    answers: {
+      letter: "",
+      answersTxt: ``,
+      isCodeBlock: "",
+    },
+  },
+  question6: {
+    question: {
+      number: "",
+      headerTxt: "",
+      codeBlock: ``,
+      footerTxt: "",
+    },
+    answers: {
+      letter: "",
+      answersTxt: ``,
+      isCodeBlock: "",
+    },
+  },
+  question7: {
+    question: {
+      number: "",
+      headerTxt: "",
+      codeBlock: ``,
+      footerTxt: "",
+    },
+    answers: {
+      letter: "",
+      answersTxt: ``,
+      isCodeBlock: "",
+    },
+  },
+  question8: {
+    question: {
+      number: "",
+      headerTxt: "",
+      codeBlock: ``,
+      footerTxt: "",
+    },
+    answers: {
+      letter: "",
+      answersTxt: ``,
+      isCodeBlock: "",
+    },
+  },
+  question9: {
+    question: {
+      number: "",
+      headerTxt: "",
+      codeBlock: ``,
+      footerTxt: "",
+    },
+    answers: {
+      letter: "",
+      answersTxt: ``,
+      isCodeBlock: "",
+    },
+  },
+  question10: {
+    question: {
+      number: "",
+      headerTxt: "",
+      codeBlock: ``,
+      footerTxt: "",
+    },
+    answers: {
+      letter: "",
+      answersTxt: ``,
+      isCodeBlock: "",
+    },
+  },
+  question11: {
+    question: {
+      number: "",
+      headerTxt: "",
+      codeBlock: ``,
+      footerTxt: "",
+    },
+    answers: {
+      letter: "",
+      answersTxt: ``,
+      isCodeBlock: "",
+    },
+  },
+  question12: {
+    question: {
+      number: "",
+      headerTxt: "",
+      codeBlock: ``,
+      footerTxt: "",
+    },
+    answers: {
+      letter: "",
+      answersTxt: ``,
+      isCodeBlock: "",
+    },
+  },
+  question13: {
+    question: {
+      number: "",
+      headerTxt: "",
+      codeBlock: ``,
+      footerTxt: "",
+    },
+    answers: {
+      letter: "",
+      answersTxt: ``,
+      isCodeBlock: "",
+    },
+  },
+  question14: {
+    question: {
+      number: "",
+      headerTxt: "",
+      codeBlock: ``,
+      footerTxt: "",
+    },
+    answers: {
+      letter: "",
+      answersTxt: ``,
+      isCodeBlock: "",
+    },
+  },
+  question15: {
+    question: {
+      number: "",
+      headerTxt: "",
+      codeBlock: ``,
+      footerTxt: "",
+    },
+    answers: {
+      letter: "",
+      answersTxt: ``,
+      isCodeBlock: "",
+    },
+  },
+  question16: {
+    question: {
+      number: "",
+      headerTxt: "",
+      codeBlock: ``,
+      footerTxt: "",
+    },
+    answers: {
+      letter: "",
+      answersTxt: ``,
+      isCodeBlock: "",
+    },
+  },
+  question17: {
+    question: {
+      number: "",
+      headerTxt: "",
+      codeBlock: ``,
+      footerTxt: "",
+    },
+    answers: {
+      letter: "",
+      answersTxt: ``,
+      isCodeBlock: "",
+    },
+  },
+  question18: {
+    question: {
+      number: "",
+      headerTxt: "",
+      codeBlock: ``,
+      footerTxt: "",
+    },
+    answers: {
+      letter: "",
+      answersTxt: ``,
+      isCodeBlock: "",
+    },
+  },
+  question19: {
+    question: {
+      number: "",
+      headerTxt: "",
+      codeBlock: ``,
+      footerTxt: "",
+    },
+    answers: {
+      letter: "",
+      answersTxt: ``,
+      isCodeBlock: "",
+    },
+  },
+  question20: {
+    question: {
+      number: "",
+      headerTxt: "",
+      codeBlock: ``,
+      footerTxt: "",
+    },
+    answers: {
+      letter: "",
+      answersTxt: ``,
+      isCodeBlock: "",
+    },
+  },
+};
+
+export default {
+  name: "JavaScript",
+  tests: [test1],
+};
