73ec9249bfcaedb4228ac4d473deb1c8acb9b32e
Dimitri Williams <dimtri.williams.6448@gmail.com>
2024-02-18 13:45:15 -0600

    Feat: Setup base skeleton structure for TestSelectPage component.
    Setup basic layout to play around with later on.
    
    Layout for TestSelectPage includes Box components that contain links to the respective test pages.

--- src/pages/TestSelectPage.js
+++ src/pages/TestSelectPage.js
@@ -1,5 +1,36 @@
+import { Box, VStack } from "@chakra-ui/react";
+import { Link } from "wouter";
+
 const TestSelectPage = () => {
-  return <div>Test Select Page</div>;
+  const testLinks = [
+    { title: "JavaScript", link: "javascript" },
+    { title: "JavaScript ES6", link: "javascript-es6" },
+    { title: "JavaScript Patterns", link: "javascript-patterns" },
+    { title: "Graphql Apollo", link: "graphql-apollo" },
+    { title: "Git", link: "git" },
+    { title: "React", link: "react" },
+  ];
+
+  const testSelectBoxProps = {
+    textAlign: "center",
+    padding: "4",
+    bg: "blue.400",
+    color: "black",
+    maxW: "full",
+    borderRadius: "lg",
+  };
+
+  const testSelect = testLinks.map((testLink) => (
+    <Box {...testSelectBoxProps}>
+      <Link href={testLink.link}>{testLink.title}</Link>
+    </Box>
+  ));
+
+  return (
+    <VStack VStack w="full" h="full" p={10} spacing={6} align="flex-center">
+      {testSelect}
+    </VStack>
+  );
 };
 
 export default TestSelectPage;
