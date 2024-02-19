dbafd3c216fab5d67177037e47b7bd7e2796998b
Dimitri Williams <dimtri.williams.6448@gmail.com>
2024-02-19 13:58:37 -0600

    Feat: Add logos and title for TestSelectCard.

--- src/components/TestSelectCardList.js
+++ src/components/TestSelectCardList.js
@@ -1,18 +1,36 @@
 import { SimpleGrid } from "@chakra-ui/react";
 import { Link } from "wouter";
 import { TestSelectCard } from ".";
+import apolloLogoPath from "../logos/apollo-graphql-compact.svg";
+import gitLogoPath from "../logos/git-icon.svg";
+import javaScriptEsLogoPath from "../logos/logo-javascript.svg";
+import javaScriptLogoPath from "../logos/javascript-1.svg";
+import javaScriptPatternsLogoPath from "../logos/javascript-2.svg";
+import reactLogoPath from "../logos/react-1.svg";
 
 const TestSelectCardList = () => {
-  const testCards = [
-    { title: "JavaScript", link: "javascript" },
-    { title: "JavaScript ES6", link: "javascript-es6" },
-    { title: "JavaScript Patterns", link: "javascript-patterns" },
-    { title: "Graphql Apollo", link: "graphql-apollo" },
-    { title: "Git", link: "git" },
-    { title: "React", link: "react" },
+  const testSelectCardsProps = [
+    { id: 1, image: apolloLogoPath, title: "Apollo Graphql", description: "" },
+    { id: 2, image: gitLogoPath, title: "Git", description: "" },
+    {
+      id: 3,
+      image: javaScriptEsLogoPath,
+      title: "JavaScript ES6",
+      description: "",
+    },
+    { id: 4, image: javaScriptLogoPath, title: "JavaScript", description: "" },
+    {
+      id: 5,
+      image: javaScriptPatternsLogoPath,
+      title: "JavaScript Patterns",
+      description: reactLogoPath,
+    },
+    { id: 6, image: reactLogoPath, title: "React", description: "" },
   ];
 
-  const testSelect = testCards.map((testCard) => <TestSelectCard />);
+  const testSelect = testSelectCardsProps.map((testSelectCardsProp) => (
+    <TestSelectCard key={testSelectCardsProp.id} {...testSelectCardsProp} />
+  ));
 
   return (
     <SimpleGrid columns={3} spacing={10}>
--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,3 @@
 export { default as TestSelectCard } from "./TestSelectCard";
-export { default as TestSelectList } from "./TestSelectList";
+export { default as TestSelectCardList } from "./TestSelectCardList";
 export { default as Layout } from "./Layout";
