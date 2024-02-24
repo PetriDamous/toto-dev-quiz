5239bac414c0a5329b184ea0253519901c29be11
Dimitri Williams <dimtri.williams.6448@gmail.com>
2024-02-24 11:02:13 -0600

    Feat: Setup basic layout for QuizModalFooterContent.

--- src/components/QuizModalFooterContent.js
+++ src/components/QuizModalFooterContent.js
@@ -1,5 +1,24 @@
+import { Button, ButtonGroup, Flex, Spacer, Text } from "@chakra-ui/react";
+
 const QuizModalFooterContent = () => {
-  return "I am a footer";
+  const questionCountSizes = { sm: "1.5rem", md: "2rem" };
+  const btnSizes = ["sm", "md", "lg"];
+
+  return (
+    <Flex alignItems="center" w="100%">
+      <Text fontSize={questionCountSizes}>0 of 20 Questions</Text>
+
+      <Spacer />
+      <ButtonGroup>
+        <Button variant="solid" colorScheme="blue" size={btnSizes}>
+          Prev
+        </Button>
+        <Button variant="solid" colorScheme="blue" size={btnSizes}>
+          Next
+        </Button>
+      </ButtonGroup>
+    </Flex>
+  );
 };
 
 export default QuizModalFooterContent;
